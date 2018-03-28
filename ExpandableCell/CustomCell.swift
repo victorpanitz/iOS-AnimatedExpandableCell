5eee4a3d87ed0d722c4bde89b8e301e21f851185
Victor Magalhães <victorpanitz@gmail.com>
2018-03-28 00:29:15 -0300

    Initial Commit

---
+++ ExpandableCell/CustomCell.swift
@@ -0,0 +1,105 @@
+//
+//  CustomCell.swift
+//  ExpandableCell
+//
+//  Created by Victor Magalhaes on 27/03/2018.
+//  Copyright © 2018 Victor Magalhaes. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CustomCell : UITableViewCell {
+    
+    var label : UILabel = {
+        let mLabel = UILabel()
+        mLabel.textColor = UIColor.black
+        mLabel.font = UIFont.systemFont(ofSize: 12, weight: .semibold)
+        mLabel.textAlignment = .left
+        mLabel.numberOfLines = 2
+        mLabel.minimumScaleFactor = 0.5
+        mLabel.adjustsFontSizeToFitWidth = true
+        return mLabel
+    }()
+    
+    var containerView : UIView = {
+       let mContainerView = UIView()
+        return mContainerView
+    }()
+    
+    var arrowImg : UIImageView = {
+        var mImage = UIImageView()
+        mImage.image = UIImage(named: "up-arrow")!.withRenderingMode(.alwaysTemplate)
+        mImage.contentMode = .scaleAspectFit
+        mImage.tintColor = UIColor.black
+        mImage.clipsToBounds = true
+        return mImage
+    }()
+    
+    var mTextField : UITextField = {
+        let mTextField = UITextField()
+        mTextField.placeholder = "Insert some label"
+        mTextField.font = UIFont.systemFont(ofSize: 12)
+        mTextField.textColor = UIColor.black
+        mTextField.backgroundColor = UIColor.white
+        mTextField.keyboardType = .alphabet
+        mTextField.textAlignment = .center
+        return mTextField
+    }()
+    
+    var isExpanded = false
+    var cellHeight: CGFloat = 44
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String!)        {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupCell()
+    }
+    
+    func setupCell(){
+        self.addSubview(label)
+        label.translatesAutoresizingMaskIntoConstraints = false
+
+        self.addSubview(arrowImg)
+        arrowImg.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.addSubview(containerView)
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        
+        containerView.addSubview(mTextField)
+        mTextField.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        NSLayoutConstraint.activate([
+            
+            label.topAnchor.constraint(equalTo: self.topAnchor),
+            label.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 8),
+            label.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            label.heightAnchor.constraint(equalToConstant: 44),
+            
+            arrowImg.topAnchor.constraint(equalTo: self.topAnchor, constant: 14),
+            arrowImg.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -8),
+            arrowImg.heightAnchor.constraint(equalToConstant: 16),
+            arrowImg.widthAnchor.constraint(equalToConstant: 16),
+            
+            containerView.topAnchor.constraint(equalTo: self.label.bottomAnchor),
+            containerView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+            containerView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            containerView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            
+            mTextField.topAnchor.constraint(equalTo: self.containerView.topAnchor),
+            mTextField.bottomAnchor.constraint(equalTo: self.containerView.bottomAnchor, constant: -8),
+            mTextField.leadingAnchor.constraint(equalTo: self.containerView.leadingAnchor, constant: 8),
+            mTextField.trailingAnchor.constraint(equalTo: self.containerView.trailingAnchor, constant: -8),
+        
+        ])
+        
+        mTextField.layer.cornerRadius = 4
+    
+    }
+    
+    required init(coder aDecoder: NSCoder){
+        super.init(coder: aDecoder)!
+    }
+
+    
+}
---
+++ ExpandableCell/ViewController.swift
@@ -0,0 +1,99 @@
+//
+//  ViewController.swift
+//  ExpandableCell
+//
+//  Created by Victor Magalhaes on 26/03/2018.
+//  Copyright © 2018 Victor Magalhaes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let newTableView : UITableView = {
+        let mTableView = UITableView()
+        mTableView.tableFooterView = UIView()
+        mTableView.register(CustomCell.self, forCellReuseIdentifier: "mCell")
+        return mTableView
+    }()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.newTableView.delegate = self
+        self.newTableView.dataSource = self
+        setupLayout()
+    }
+
+    func setupLayout(){
+        self.view.addSubview(newTableView)
+        newTableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            self.newTableView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+            self.newTableView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
+            self.newTableView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            self.newTableView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor)
+        ])
+    }
+    
+    func updateCell(_ indexPath: IndexPath){
+        self.newTableView.deselectRow(at: indexPath, animated: true)
+        if (self.newTableView.cellForRow(at: indexPath) as! CustomCell).isExpanded {
+            UIView.animate(withDuration: 0.5, delay: 0.1, options: .curveEaseOut, animations: {
+                (self.newTableView.cellForRow(at: indexPath) as! CustomCell).frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: 44)
+                (self.newTableView.cellForRow(at: indexPath) as! CustomCell).arrowImg.transform = CGAffineTransform.identity
+                (self.newTableView.cellForRow(at: indexPath) as! CustomCell).layoutIfNeeded()
+            }, completion: { (finished: Bool)  in
+                    (self.newTableView.cellForRow(at: indexPath) as! CustomCell).cellHeight = 44
+                    self.newTableView.reloadData()
+            })
+        }else{
+            UIView.animate(withDuration: 0.5, delay: 0.1, options: .curveEaseIn, animations: {
+                (self.newTableView.cellForRow(at: indexPath) as! CustomCell).frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: 88)
+                (self.newTableView.cellForRow(at: indexPath) as! CustomCell).arrowImg.transform = CGAffineTransform(rotationAngle: (180.0 * CGFloat(Double.pi)) / 180.0)
+                (self.newTableView.cellForRow(at: indexPath) as! CustomCell).layoutIfNeeded()
+            }, completion: { (finished: Bool)  in
+                (self.newTableView.cellForRow(at: indexPath) as! CustomCell).cellHeight = 88
+                self.newTableView.reloadData()
+            })
+            
+        }
+        (self.newTableView.cellForRow(at: indexPath) as! CustomCell).isExpanded = !(self.newTableView.cellForRow(at: indexPath) as! CustomCell).isExpanded
+
+    }
+    
+    
+}
+
+extension ViewController : UITableViewDataSource, UITableViewDelegate {
+ 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "mCell", for: indexPath) as! CustomCell
+        cell.backgroundColor = UIColor.lightGray
+        cell.label.text = "Name: Victor Panitz Magalhães"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return (self.newTableView.cellForRow(at: indexPath) as? CustomCell)?.cellHeight != nil ?
+            (self.newTableView.cellForRow(at: indexPath) as! CustomCell).cellHeight :
+            CGFloat(44)
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.updateCell(indexPath)
+    }
+
+   
+    
+}
+
+
+
+
+
